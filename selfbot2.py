f37fc7bb8b483c4c1581e46c34b29e79241efa29
nationalsecurityact <142487914+nationalsecurityact@users.noreply.github.com>
2023-10-13 17:28:35 +0200

    Add files via upload

---
+++ selfbot2.py
@@ -0,0 +1,65 @@
+import asyncio
+import discord
+import time
+import subprocess
+import requests
+import string
+import random
+import aiohttp
+import io
+import pyscreenshot
+import json
+from discord.ext import commands
+from discord.utils import get
+
+
+prefix = "."
+token = "MTE0OTgyODEyNjc2MzkwOTI4MA.GQBvv0.qrOXDcbqc3akyy7of82Fnp-4Sk4mxqXaYqmUsU"
+Lapsus = commands.Bot(description='Lapsus', command_prefix='.', self_bot=True)
+
+
+Lapsus.lockgc = []
+Lapsus.headers = {
+    'authorization': token,
+}
+
+@Lapsus.event
+async def lockloop():
+    while True:
+        if not Lapsus.lockgc == []:
+            for gcid in Lapsus.lockgc:
+                response = requests.put(f'https://discordapp.com/api/v8/channels/{gcid}/recipients/1337',headers=Lapsus.headers)
+                if response.status_code == 429:
+                    response_c = response.text
+                    response_content = json.loads(response_c)
+                    lockedsec = response_content.get('retry_after')
+                else:
+                    headers = {"Authorization": token}
+                    for i in range(30):
+                        response = requests.put(f'https://discordapp.com/api/v8/channels/{gcid}/recipients/1337',headers=Lapsus.headers)
+        await asyncio.sleep(0.8)
+
+
+@Lapsus.event
+async def on_connect():
+  print(f"Connected at {token} !")
+  await lockloop()
+
+
+@Lapsus.command()
+async def antiveski(ctx, groupid):
+    await ctx.send('__**Anti Veski Activé 🤓 !**__')
+    Lapsus.lockgc.extend([groupid] * 1)
+
+@Lapsus.command()
+async def unlock(ctx, groupid=None):
+    await ctx.send('__**Anti Veski Desactivé 🤓 !**__')
+    if groupid:
+        Lapsus.lockgc.pop(groupid)
+        print(f'Unlocking {groupid} in 120 seconds Master !')
+    else:
+        Lapsus.lockgc.clear()
+        print(f'Unlocked All Groups')
+
+
+Lapsus.run(token, bot=False, reconnect=True)
